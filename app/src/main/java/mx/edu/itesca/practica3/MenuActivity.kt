529108f4c1085f8e71dd4266721a2133028eedec
Multimedia <nazobx@gmail.com>
2025-02-14 09:50:14 -0700

    practica 6

--- app/src/main/java/mx/edu/itesca/practica3/MenuActivity.kt
+++ app/src/main/java/mx/edu/itesca/practica3/MenuActivity.kt
@@ -1,6 +1,8 @@
 package mx.edu.itesca.practica3
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,35 @@ class MenuActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        var btn_antojitos:Button=findViewById(R.id.btn_cold_drinks) as Button
+        var btn_sopas:Button=findViewById(R.id.btn_combos) as Button
+        var btn_tortas:Button=findViewById(R.id.btn_salties) as Button
+        var btn_bebidas:Button=findViewById(R.id.btn_custom) as Button
+
+        btn_antojitos.setOnClickListener {
+            var intent:Intent =Intent(this,ProductActivity::class.java)
+            intent.putExtra("menuType","Antojitos")
+            startActivity(intent)
+        }
+
+        btn_sopas.setOnClickListener {
+            var intent:Intent =Intent(this,ProductActivity::class.java)
+            intent.putExtra("menuType","Sopas")
+            startActivity(intent)
+        }
+
+        btn_tortas.setOnClickListener {
+            var intent:Intent =Intent(this,ProductActivity::class.java)
+            intent.putExtra("menuType","Tortas")
+            startActivity(intent)
+        }
+
+        btn_bebidas.setOnClickListener {
+            var intent:Intent =Intent(this,ProductActivity::class.java)
+            intent.putExtra("menuType","Bebidas")
+            startActivity(intent)
         }
     }
 }
